0e91cc0a5714ee58b82fd095f4db519454826295
Dominik Rodler <dominik.rodler@gmail.com>
2018-10-11 11:00:27 +0200

    ignore trendmicro eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 const path = require('path');
 
 module.exports = {
-    extends: 'trendmicro',
-    parser: 'babel-eslint',
-    env: {
-        browser: true,
-        node: true
+  // extends: 'trendmicro',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            extensions: ['.js', '.jsx'],
+          },
+        },
+      },
     },
-    settings: {
-        'import/resolver': {
-            webpack: {
-                config: {
-                    resolve: {
-                        modules: [
-                            path.resolve(__dirname, 'src'),
-                            'node_modules'
-                        ],
-                        extensions: ['.js', '.jsx']
-                    }
-                }
-            }
-        }
-    },
-    rules: {
-        'react/jsx-no-bind': [1, {
-            allowArrowFunctions: true
-        }],
-        'react/prefer-stateless-function': 0,
-        'react/no-access-state-in-setstate': 0
-    }
+  },
+  rules: {
+    'react/jsx-no-bind': [
+      1,
+      {
+        allowArrowFunctions: true,
+      },
+    ],
+    'react/prefer-stateless-function': 0,
+    'react/no-access-state-in-setstate': 0,
+  },
 };
