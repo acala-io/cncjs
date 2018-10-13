ffd900bf5d4e7ccf343c4628d7cec006ee7a1d41
Dominik Rodler <dominik.rodler@gmail.com>
2018-10-13 20:04:07 +0200

    maybe for later

--- src/web/store/index.js
+++ src/web/store/index.js
@@ -1,11 +1,12 @@
 import {applyMiddleware, compose, createStore} from 'redux';
 import createDebounce from 'redux-debounced';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 
+// import API from '../api/index';
 import rootReducer from '../reducers/index';
 
-const middlewares = [createDebounce()];
+const middlewares = [createDebounce()]; // , thunk.withExtraArgument(API)];
 
 // Add redux logger in dev environment
 if (process.env.NODE_ENV === 'development') {
