b376895df2f1faca102e3615e5aee209a23db5d5
Dominik Rodler <dominik.rodler@gmail.com>
2018-10-17 11:03:00 +0200

    Added SplitButton component for wrapping multiple buttons

---
+++ src/web/components_new/SplitButton.js
@@ -0,0 +1,26 @@
+/*
+ * Split Button component for wrapping multiple buttons.
+ *
+ * Usage:
+ * <SplitButton className="">
+ *   <Button text="Save" size="large" isDisabled={!this.isValid} handleClick={() => { alert('hi'); }}/>
+ *   <Button text="Save" width="full-width" isInProgress={this.isLoading} handleClick={() => { alert('hi'); }}/>
+ * </SplitButton>
+ */
+
+import classcat from 'classcat';
+import React from 'react';
+import {arrayOf, node, oneOfType, string} from 'prop-types';
+
+const SplitButton = ({children, className = ''}) => {
+  const classes = classcat(['split-button', className]);
+
+  return <div className={classes}>{children}</div>;
+};
+
+SplitButton.propTypes = {
+  className: string,
+  children: oneOfType([arrayOf(node), node]).isRequired,
+};
+
+export default SplitButton;
