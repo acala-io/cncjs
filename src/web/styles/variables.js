e6cb4aee96018fb1df548ede703ef2680197a865
Dominik Rodler <dominik.rodler@gmail.com>
2018-10-17 19:10:23 +0200

    Added JS object with constants for styling

---
+++ src/web/styles/variables.js
@@ -0,0 +1,117 @@
+/*
+ * Global variables.
+ */
+
+const transitionTimeMultiplier = 1; // scaling factor for development / debugging
+
+export default {
+  color: {
+    primary: {
+      default: 'hsl(248, 67%, 64%)',
+      darker: 'darken(saturate($primary, 3%), 8%)',
+      darkest: 'darken(saturate($primary, 5%), 42%)',
+      lighter: 'lighten(desaturate($primary, 8%), 8%)',
+      lightest: 'lighten(desaturate($primary, 13%), 34%)',
+    },
+    secondary: {
+      default: 'hsl(100, 67%, 64%)',
+      darker: 'darken(saturate($secondary, 3%), 13%)',
+      darkest: 'darken(saturate($secondary, 5%), 34%)',
+      lighter: 'lighten(desaturate($secondary, 8%), 13%)',
+      lightest: 'lighten(desaturate($secondary, 13%), 34%)',
+    },
+    clickable: {
+      // colors for clickable items (links, buttons, etc.)
+      default: 'hsl(207, 67%, 64%)',
+      highlight: 'lighten(saturate($clickable, 13%), 5%)',
+      background: 'transparentize($clickable--highlight, 0.72)',
+    },
+    text: {
+      default: 'hsl(201, 13%, 21%)',
+      lighter: 'lighten(desaturate($text, 5%), 55%)',
+      lightest: 'lighten(desaturate($text, 5%), 68%)',
+      inverse: 'hsl(0, 0%, 100%)',
+    },
+    background: {
+      default: 'hsl(201, 2%, 92%)',
+      darker: 'darken(saturate($background, 3%), 16%)',
+      darkest: 'darken(saturate($background, 3%), 55%)',
+      slightlyOffBlack: 'hsl(201, 2%, 8%)',
+      lighter: 'hsl(201, 2%, 94%)',
+      slightlyOffWhite: 'hsl(201, 1%, 97%)',
+      white: 'hsl(0, 0%, 100%)',
+      highlight: 'hsl(201, 100%, 94%)',
+      warning: 'hsl(4, 90%, 94%)',
+    },
+    border: {
+      default: 'hsl(201, 13%, 79%)',
+      darker: 'darken($border, 8%)',
+      lighter: 'lighten($border, 8%)',
+      lightest: 'lighten($border, 13%)',
+    },
+    state: {
+      danger: 'hsl(0, 88%, 66%)',
+      error: 'hsl(1, 62%, 58%)',
+      success: 'hsl(121, 201%, 55%)',
+      attention: 'hsl(34, 82%, 64%)',
+    },
+    transparent: 'transparent',
+  },
+  border: {
+    radius: {
+      default: '3px',
+      tiny: '2px',
+      small: '1px',
+      large: '$border-radius * 2',
+      huge: '$border-radius * 3',
+      circle: '50%',
+      pill: '999px',
+    },
+    width: {
+      default: '1px',
+      strong: '2px',
+    },
+  },
+  font: {
+    size: {
+      default: '1rem', // = 14px $inuit-base-font-size
+      tiny: '0.714rem',
+      small: '0.857rem',
+      large: '1.5rem',
+      huge: '2rem',
+      pageTitle: '4rem',
+    },
+    weight: {
+      normal: '400',
+      bold: '700',
+    },
+  },
+  transition: {
+    time: {
+      veryFast: `${75 * transitionTimeMultiplier}ms`,
+      fast: `${150 * transitionTimeMultiplier}ms`,
+      medium: `${250 * transitionTimeMultiplier}ms`,
+      slow: `${400 * transitionTimeMultiplier}ms`,
+      verySlow: `${1000 * transitionTimeMultiplier}ms`,
+    },
+  },
+  zIndex: {
+    recessed: -1,
+    base: 0,
+
+    // elements on the base layer that need to stick out
+    elevated1: 10,
+    elevated2: 11,
+    elevated3: 12,
+
+    // elements that float over the base layer
+    overlayed1: 100,
+    overlayed2: 110,
+    overlayed3: 120,
+
+    // elements that must never be covered
+    topmost1: 1000000,
+    topmost2: 1000010,
+    topmost3: 1000020,
+  },
+};
--- src/web/widgets/Axes/components/AxisLabel.js
+++ src/web/widgets/Axes/components/AxisLabel.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import s from '../../../styles/variables';
+
 const AxisLabel = styled(props => <div {...props} />)`
   // @include center--xy;
-  // color: transparentize($text--inverse, 0.4);
-  // font-size: $font-size--huge;
-
   left: 50%;
   position: absolute;
   top: 50%;
   transform: translate(-50%, -50%);
-  color: rgba(255, 255, 255, 0.6);
-  font-size: 2rem;
+
+  color: rgba(255, 255, 255, 0.5); // transparentize(s.color.text.inverse, 0.4);
+  font-size: ${s.font.size.huge};
 `;
 
 export default AxisLabel;
