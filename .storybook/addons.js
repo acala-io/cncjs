10025a0ed95c671cd13a9a6c270f16a3e26e47e5
Dominik Rodler <dominik.rodler@gmail.com>
2018-10-20 01:13:20 +0200

    Removed storybook screenshots addon
    Needs further configuration to work and Chromium is heavy on npm installs

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,5 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-backgrounds/register';
-import 'storybook-chrome-screenshot/register';
+// import 'storybook-chrome-screenshot/register';
 // import '@storybook/addon-viewport/register';
--- stories/ButtonGroup.stories.js
+++ stories/ButtonGroup.stories.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {action} from '@storybook/addon-actions';
 import {storiesOf} from '@storybook/react';
-// import {withKnobs, text} from '@storybook/addon-knobs/react';
 
 import backgroundsDecorator from './lib/backgroundsDecorator';
-import screenshotsDecorator from './lib/screenshotsDecorator';
 
 import ButtonGroup from '../src/web/components_new/ButtonGroup';
 
 storiesOf('Components/ButtonGroup', module)
   .addDecorator(backgroundsDecorator)
-  .addDecorator(screenshotsDecorator)
-  // .addDecorator(withKnobs)
   .add('Default', () => (
     <ButtonGroup
       optionName="whatever"
@@ -20,19 +16,15 @@ storiesOf('Components/ButtonGroup', module)
       onChange={action('selected')}
     />
   ))
-  .add('With units', () => {
-    // const selectedValue = text('Selected Value', '1');
-
-    return (
-      <ButtonGroup
-        optionName="whatever"
-        options={[
-          {value: 0.01, label: 0.01, unit: 'mm'},
-          {value: 0.1, label: 0.1, unit: 'mm'},
-          {value: 1, label: 1, unit: 'mm'},
-        ]}
-        selectedValue={1}
-        onChange={action('selected')}
-      />
-    );
-  });
+  .add('With units', () => (
+    <ButtonGroup
+      optionName="whatever"
+      options={[
+        {value: 0.01, label: 0.01, unit: 'mm'},
+        {value: 0.1, label: 0.1, unit: 'mm'},
+        {value: 1, label: 1, unit: 'mm'},
+      ]}
+      selectedValue={1}
+      onChange={action('selected')}
+    />
+  ));
