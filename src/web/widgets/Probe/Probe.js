e94795dff3e53ed1c9f2fa2323f73ce8e7e36586
Dominik Rodler <dominik.rodler@gmail.com>
2018-10-21 16:11:28 +0200

    Split Probe widget code in smaller chunks

--- src/web/widgets/Probe/Probe.js
+++ src/web/widgets/Probe/Probe.js
@@ -18,11 +18,33 @@ class Probe extends PureComponent {
 
   render() {
     const {actions, state} = this.props;
-    const {canClick, units, probeCommand, probeDepth, probeFeedrate, touchPlateHeight, retractionDistance} = state;
+    const {canClick} = state;
 
-    const displayUnits = units === METRIC_UNITS ? i18n._('mm') : i18n._('in');
-    const feedrateUnits = units === METRIC_UNITS ? i18n._('mm/min') : i18n._('in/min');
-    const step = units === METRIC_UNITS ? 1 : 0.1;
+    return (
+      <div>
+        {this.probeCommand}
+        <div>
+          {this.probeDepth}
+          {this.feedrate}
+          {this.touchPlateHeight}
+          {this.retractionDistance}
+        </div>
+        <div>
+          <Button
+            text={i18n._('Probe Z-Position')}
+            isDisabled={!canClick}
+            onClick={() => {
+              actions.openModal(MODAL_PREVIEW);
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  get probeCommand() {
+    const {changeProbeCommand} = this.props.actions;
+    const {probeCommand} = this.props.state;
 
     let probeCommandHint;
     switch (probeCommand) {
@@ -44,97 +66,120 @@ class Probe extends PureComponent {
     }
 
     return (
-      <div>
-        <div className="form-group">
-          <label className="control-label">{i18n._('Probe Command')}</label>
-          <ButtonGroup
-            optionName="probe-command"
-            options={['G38.2', 'G38.3', 'G38.4', 'G38.5']}
-            selectedValue={probeCommand}
-            onChange={actions.changeProbeCommand}
+      <div className="form-group">
+        <label className="control-label">{i18n._('Probe Command')}</label>
+        <ButtonGroup
+          optionName="probe-command"
+          options={['G38.2', 'G38.3', 'G38.4', 'G38.5']}
+          selectedValue={probeCommand}
+          onChange={changeProbeCommand}
+        />
+        <Hint className="u-margin-top-tiny" block>
+          {probeCommandHint}
+        </Hint>
+      </div>
+    );
+  }
+
+  get probeDepth() {
+    const {handleProbeDepthChange} = this.props.actions;
+    const {probeDepth, units} = this.props.state;
+
+    const displayUnits = units === METRIC_UNITS ? i18n._('mm') : i18n._('in');
+    const step = units === METRIC_UNITS ? 1 : 0.1;
+
+    return (
+      <div className="form-group">
+        <label className="control-label">{i18n._('Probe Depth')}</label>
+        <div className="input-group input-group-sm">
+          <input
+            type="number"
+            className="form-control"
+            value={probeDepth}
+            placeholder="0.00"
+            min={0}
+            step={step}
+            onChange={handleProbeDepthChange}
           />
-          <Hint className="u-margin-top-tiny" block>
-            {probeCommandHint}
-          </Hint>
+          <div className="input-group-addon">{displayUnits}</div>
         </div>
-        <div className="row no-gutters">
-          <div className="col-xs-6" style={{paddingRight: 5}}>
-            <div className="form-group">
-              <label className="control-label">{i18n._('Probe Depth')}</label>
-              <div className="input-group input-group-sm">
-                <input
-                  type="number"
-                  className="form-control"
-                  value={probeDepth}
-                  placeholder="0.00"
-                  min={0}
-                  step={step}
-                  onChange={actions.handleProbeDepthChange}
-                />
-                <div className="input-group-addon">{displayUnits}</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6" style={{paddingLeft: 5}}>
-            <div className="form-group">
-              <label className="control-label">{i18n._('Probe Feedrate')}</label>
-              <div className="input-group input-group-sm">
-                <input
-                  type="number"
-                  className="form-control"
-                  value={probeFeedrate}
-                  placeholder="0.00"
-                  min={0}
-                  step={step}
-                  onChange={actions.handleProbeFeedrateChange}
-                />
-                <span className="input-group-addon">{feedrateUnits}</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6" style={{paddingRight: 5}}>
-            <div className="form-group">
-              <label className="control-label">{i18n._('Touch Plate Thickness')}</label>
-              <div className="input-group input-group-sm">
-                <input
-                  type="number"
-                  className="form-control"
-                  value={touchPlateHeight}
-                  placeholder="0.00"
-                  min={0}
-                  step={step}
-                  onChange={actions.handleTouchPlateHeightChange}
-                />
-                <span className="input-group-addon">{displayUnits}</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6" style={{paddingLeft: 5}}>
-            <div className="form-group">
-              <label className="control-label">{i18n._('Retraction Distance')}</label>
-              <div className="input-group input-group-sm">
-                <input
-                  type="number"
-                  className="form-control"
-                  value={retractionDistance}
-                  placeholder="0.00"
-                  min={0}
-                  step={step}
-                  onChange={actions.handleRetractionDistanceChange}
-                />
-                <span className="input-group-addon">{displayUnits}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row no-gutters">
-          <Button
-            text={i18n._('Probe Z-Position')}
-            isDisabled={!canClick}
-            onClick={() => {
-              actions.openModal(MODAL_PREVIEW);
-            }}
+      </div>
+    );
+  }
+
+  get feedrate() {
+    const {handleProbeFeedrateChange} = this.props.actions;
+    const {probeFeedrate, units} = this.props.state;
+
+    const feedrateUnits = units === METRIC_UNITS ? i18n._('mm/min') : i18n._('in/min');
+    const step = units === METRIC_UNITS ? 1 : 0.1;
+
+    return (
+      <div className="form-group">
+        <label className="control-label">{i18n._('Probe Feedrate')}</label>
+        <div className="input-group input-group-sm">
+          <input
+            type="number"
+            className="form-control"
+            value={probeFeedrate}
+            placeholder="0.00"
+            min={0}
+            step={step}
+            onChange={handleProbeFeedrateChange}
           />
+          <span className="input-group-addon">{feedrateUnits}</span>
+        </div>
+      </div>
+    );
+  }
+
+  get touchPlateHeight() {
+    const {handleTouchPlateHeightChange} = this.props.actions;
+    const {touchPlateHeight, units} = this.props.state;
+
+    const displayUnits = units === METRIC_UNITS ? i18n._('mm') : i18n._('in');
+    const step = units === METRIC_UNITS ? 1 : 0.1;
+
+    return (
+      <div className="form-group">
+        <label className="control-label">{i18n._('Touch Plate Thickness')}</label>
+        <div className="input-group input-group-sm">
+          <input
+            type="number"
+            className="form-control"
+            value={touchPlateHeight}
+            placeholder="0.00"
+            min={0}
+            step={step}
+            onChange={handleTouchPlateHeightChange}
+          />
+          <span className="input-group-addon">{displayUnits}</span>
+        </div>
+      </div>
+    );
+  }
+
+  get retractionDistance() {
+    const {handleRetractionDistanceChange} = this.props.actions;
+    const {retractionDistance, units} = this.props.state;
+
+    const displayUnits = units === METRIC_UNITS ? i18n._('mm') : i18n._('in');
+    const step = units === METRIC_UNITS ? 1 : 0.1;
+
+    return (
+      <div className="form-group">
+        <label className="control-label">{i18n._('Retraction Distance')}</label>
+        <div className="input-group input-group-sm">
+          <input
+            type="number"
+            className="form-control"
+            value={retractionDistance}
+            placeholder="0.00"
+            min={0}
+            step={step}
+            onChange={handleRetractionDistanceChange}
+          />
+          <span className="input-group-addon">{displayUnits}</span>
         </div>
       </div>
     );
